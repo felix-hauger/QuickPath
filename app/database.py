689472affc021b81a8e675baa6efe2c426d87be4
felix-hauger <felix.hauger@laplateforme.io>
2025-10-01 20:58:40 +0200

    fix(db): set check_same_thread only for SQLite
    Add condition so that the SQLAlchemy argument `check_same_thread`
    is set to `False` only if the database type is SQLite. This prevents
    issues for other database types that don’t require this setting.

--- app/database.py
+++ app/database.py
@@ -2,7 +2,14 @@ from sqlmodel import SQLModel, create_engine, Session
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./shortener.db")
-engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 def get_session():
     with Session(engine) as session:
